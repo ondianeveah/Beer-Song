2c1f9f0d45cbc821b8669791e3959c09cda4973d
Ondia Brown <Ondianeveah19@gmail.com>
2020-03-11 17:00:06 -0700

    First Project that I actually understood (3/11/2020

---
+++ src/main/java/firstpackage/Main.java
@@ -0,0 +1,60 @@
+package firstpackage;
+
+public class Main {
+    // private int x; - within class
+    //Created a method
+    //static int y; (5 players(objects), and 1 ball(this variable is that 1 ball shared among players)
+    //public static void printUserName(String userName) {
+        //System.out.println("Welcome to Wakanda " + userName);
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 35;
+        int z = 15;
+        int intNumber =23;
+        long longNumber = intNumber;
+        System.out.println(longNumber);
+        float dollar = 12f;
+        float dollar2 = 3f;
+        double speed = 3000000d;
+        double speed2 = 52d;
+        System.out.println(speed/speed2);
+        System.out.println(dollar/dollar2);
+        System.out.println(y);
+        System.out.println(x);
+        System.out.println((x + z) * y);
+        System.out.println("/n" + x%y);
+}
+
+
+        // We invoked or called or used our method
+       // printUserName("Crackhead Billy");
+       // printUserName("Hope you ain't got the Corona Virus");
+      //  multiplyNum(10, 5);
+    //    math(50, 5, 2);
+      //  totalDiscount(300, 30);
+    }
+
+   // public static void totalDiscount(int price, float  discount) {
+        // int- allows whole numbers
+        // float - allows decimal(smaller range, f follows)
+        // double - allows dec. (larger range)
+   //     float decimal = discount/100;
+     //   float moneyOff = price * decimal;
+        // gives number of actual money taken off of price
+    //    float total = price - moneyOff;
+       // System.out.println(total);
+
+
+   // public static void math(int A, int B, int C) {
+        // local variables- can't be used outside of placement
+      //  System.out.println((A - B) / C);
+
+
+   // public static void multiplyNum(int number, int number2) {
+      //  System.out.println(number * number2);
+
+
+//boolean-true/false
+//
+
+
